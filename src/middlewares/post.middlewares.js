3049b5f77a30d8764c81ce472d3f998ac6bd1e4c
Lucas Ximenes <lucasdximenes@gmail.com>
2022-12-05 16:13:10 -0300

    feat: :rocket: Create middlewares function "validatePostUpdateBody" to validate the body of requisition to update a post

--- src/middlewares/post.middlewares.js
+++ src/middlewares/post.middlewares.js
@@ -1,7 +1,10 @@
 const ApiErrors = require('../helpers/apiErrors');
-const { validateCreatePostBody } = require('./validations');
+const {
+  validateCreatePostBody,
+  validateUpdatePostBody,
+} = require('./validations');
 
-const validatePostBody = (req, res, next) => {
+const validatePostBody = (req, _res, next) => {
   const { title, content, categoryIds } = req.body;
   if (!title || !content || !categoryIds) {
     throw new ApiErrors(400, 'Some required fields are missing');
@@ -10,6 +13,17 @@ const validatePostBody = (req, res, next) => {
   return next();
 };
 
+const validatePostUpdateBody = (req, _res, next) => {
+  const { title, content } = req.body;
+  if (!title || !content) {
+    throw new ApiErrors(400, 'Some required fields are missing');
+  }
+  validateUpdatePostBody(req.body);
+  return next();
+};
+
 module.exports = {
   validatePostBody,
+  validateUpdatePostBody,
+  validatePostUpdateBody,
 };
