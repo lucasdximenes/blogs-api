3095191e0c5a479bc63ea77dc2688885123e473c
Lucas Ximenes <lucasdximenes@gmail.com>
2022-12-06 17:02:07 -0300

    remove some trybe comments, config files and dependencies

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
-require('express-async-errors');
-const express = require('express');
-const routes = require('./routes');
-
-// ...
+require("express-async-errors");
+const express = require("express");
+const routes = require("./routes");
+const { errorMiddleware } = require("./middlewares");
+const morgan = require("morgan");
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan("dev"));
 app.use(routes);
 
-// ...
+app.use(errorMiddleware);
 
-// É importante exportar a constante `app`,
-// para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,15 +1,6 @@
-require('dotenv').config();
-const app = require('./app');
-const { errorMiddleware } = require('./middlewares');
+require("dotenv").config();
+const app = require("./app");
 
-// não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
 
-// não remova esse endpoint
-app.get('/', (_request, response) => {
-  response.send();
-});
-
-app.use(errorMiddleware);
-
-app.listen(port, () => console.log('ouvindo porta', port));
+app.listen(port, () => console.log("ouvindo porta", port));
