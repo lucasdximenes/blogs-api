11befe9145b24a989d6c846985765871c23ff06b
Lucas Ximenes <lucasdximenes@gmail.com>
2022-12-06 17:07:44 -0300

    add prettierrc file

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-require("express-async-errors");
-const express = require("express");
-const routes = require("./routes");
-const { errorMiddleware } = require("./middlewares");
-const morgan = require("morgan");
+require('express-async-errors');
+const express = require('express');
+const morgan = require('morgan');
+const routes = require('./routes');
+const { errorMiddleware } = require('./middlewares');
 
 const app = express();
 
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(routes);
 
 app.use(errorMiddleware);
