c1fff5801500e66bf7b3097373b0b9152fdb6659
Lucas Ximenes <lucasdximenes@gmail.com>
2022-12-08 08:25:13 -0300

    docs: :package: Add swagger-ui-express to provide document in page

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 require('express-async-errors');
 const express = require('express');
 const morgan = require('morgan');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../api-documentation.json');
 const routes = require('./routes');
 const { errorMiddleware } = require('./middlewares');
 
@@ -9,6 +11,8 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(routes);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(errorMiddleware);
 
